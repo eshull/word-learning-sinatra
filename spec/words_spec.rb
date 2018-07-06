233331b0c264f8733ed5bf178003fcef1641c2b1
Eric Shull <eshulldesign@gmail.com>
2018-07-06 14:43:42 -0700

    add positive pass for find  method

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -29,6 +29,17 @@ describe(".clear") do
     expect(Words.all()).to(eq([]))
   end
 end
+
+describe(".find") do
+    it("finds an item based on its id") do
+      words = Words.new("banana")
+      words.save()
+      words2 = Words.new("house")
+      words2.save()
+      expect(Words.find(1)).to(eq(words))
+      expect(Words.find(2)).to(eq(words2))
+    end
+end
 # describe("#things") do
 #   it("returns a list") do
 #     expect(Things.all()).to(eq([]))
